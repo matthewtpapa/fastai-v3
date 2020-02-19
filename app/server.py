5e116f9f0312790e00f93da53228eab5a94d4af0
Matt <38872171+matthewtpapa@users.noreply.github.com>
2020-02-18 23:11:28 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1QR4qpyHFWfrbGxLqiuIPIUYpX9sVGeX1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Sq3Zh-_GpfO_vMNpDtVKhA5n-0ZA-j--'
 export_file_name = 'export.pkl'
 
 classes = ['happy', 'sad', 'shocked', 'angry']
